f34258ae522ec7bbb81c05f2211ff5d4ffae8738
lcnde <176673026+lcnde@users.noreply.github.com>
2022-02-15 16:20:35 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+// messages
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date()
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('new')
+});
+
+router.post('/new', (req, res) => {
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()})
+  res.redirect('/')
+})
 
 module.exports = router;
